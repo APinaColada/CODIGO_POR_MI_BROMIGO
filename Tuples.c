a48caacda8be5eeed542665de62b7798d44e75c3
APinaColada <38115951+APinaColada@users.noreply.github.com>
2018-04-05 16:52:38 -0400

    Add files via upload

---
+++ Tuples.c
@@ -0,0 +1,63 @@
+#include "Tuples.h"
+#include <string.h>
+
+CSG *createCSG(char *course, int SID, char *grade) {
+    CSG *CSGP = malloc(sizeof(CSG));
+    CSGP->course = malloc(sizeof(char)*100);
+    strncpy(CSGP->course, course, 100);
+    CSGP->SID = SID;
+    CSGP->grade = malloc(sizeof(char)*100);
+    strncpy(CSGP->grade, grade, 100);
+    CSGP->next = NULL;
+
+    return CSGP;
+}
+
+SNAP *createSNAP(int SID, char *name, char *address, int phone) {
+    SNAP *SNAPP = malloc(sizeof(SNAP));
+    SNAPP->SID = SID;
+    SNAPP->name = malloc(sizeof(char)*100);
+    strncpy(SNAPP->name, name, 100);
+    SNAPP->address = malloc(sizeof(char)*100);
+    strncpy(SNAPP->address, address, 100);
+    SNAPP->phone = phone;
+    SNAPP->next = NULL;
+
+    return SNAPP;
+}
+
+CP *createCP(char *C, char *P) {
+    CP *CPP = malloc(sizeof(CP));
+    CPP->course = malloc(sizeof(char)*100);
+    strncpy(CPP->course, C, 100);
+    CPP->prereq = malloc(sizeof(char)*100);
+    strncpy(CPP->prereq, P, 100);
+    CPP->next = NULL;
+
+    return CPP;
+}
+
+
+CDH *createCDH(char *C, char *D, char *H) {
+    CDH *CDHP = malloc(sizeof(CDH));
+    CDHP->course = malloc(sizeof(char)*100);
+    strncpy(CDHP->course, C, 100);
+    CDHP->day = malloc(sizeof(char)*100);
+    strncpy(CDHP->day, D, 100);
+    CDHP->hour=malloc(sizeof(char)*100);
+    strncpy(CDHP->hour, H, 100);
+    CDHP->next = NULL;
+
+    return CDHP;
+}
+
+CR *createCR(char *C, char *R) {
+    CR *CRP = malloc(sizeof(CR));
+    CRP->course=malloc(sizeof(char)*100);
+    strncpy(CRP->course, C, 100);
+    CRP->room=malloc(sizeof(char)*100);
+    strncpy(CRP->room, R, 100);
+    CRP->next = NULL;
+
+    return CRP;
+}
---
+++ Tuples.h
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//define structs for each relation
+
+//Course, SID, Grade
+struct CSG {
+    char *course;
+    int SID;
+    char *grade;
+    struct CSG *next;
+};
+typedef struct CSG CSG;
+//SID, Name, Address, Phone
+struct SNAP {
+    int SID;
+    char *name;
+    char *address;
+    int phone;
+    struct SNAP *next;
+};
+typedef struct SNAP SNAP;
+//Course, Prerequisite
+struct CP {
+    char *course;
+    char *prereq;
+    struct CP *next;
+};
+typedef struct CP CP;
+//Course, Day, Hour
+struct CDH {
+    char *course;
+    char *day;
+    char *hour;
+    struct CDH *next;
+};
+typedef struct CDH CDH;
+//Course, Room
+struct CR {
+    char *course;
+    char *room;
+    struct CR *next;
+};
+typedef struct CR CR;
+
+//function prototypes
+CSG *createCSG(char *, int, char *);
+SNAP *createSNAP(int, char *, char *, int);
+CP *createCP(char *, char *);
+CDH *createCDH(char *, char *, char *);
+CR *createCR(char *, char *);
