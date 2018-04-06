61c95bae9a874390374d18729740dc44f44b35f8
APinaColada <38115951+APinaColada@users.noreply.github.com>
2018-04-06 18:45:58 -0400

    Update Tuples.h

--- Tuples.h
+++ Tuples.h
@@ -49,3 +49,10 @@ SNAP *createSNAP(int, char *, char *, char *);
 CP *createCP(char *, char *);
 CDH *createCDH(char *, char *, char *);
 CR *createCR(char *, char *);
+
+//equals functions
+int equal_SNAP(SNAP* s1, SNAP* s2);
+int equal_CSG(CSG* c1, CSG* c2);
+int equal_CP(CP* c1, CP* c2);
+int equal_CDH(CDH* c1, CDH* c2);
+int equal_CR(CR* c1, CR* c2);
