f68f4766ba1d0d8358f58c0c7ff618159fc29322
tpauly2 <tpauly2@u.rochester.edu>
2018-04-06 19:34:21 -0400

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,1 +1,17 @@
-typedef struct 
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Database {
+  void array[500];
+  int size;
+};
+typedef struct Database Database;
+
+struct Map {
+  int array[500];
+  int size;
+};
+typedef struct Map Map;
+
+int hash_function(int key);
+int size(Map map); 
