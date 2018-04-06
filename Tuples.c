e8d15f566c43c262715e2dc580500cde5cc65fe2
tpauly2 <tpauly2@u.rochester.edu>
2018-04-05 20:41:10 -0400

    Update Tuples.c

--- Tuples.c
+++ Tuples.c
@@ -5,11 +5,9 @@
 //Key: Student ID
 CSG *createCSG(char *course, int SID, char *grade) {
     CSG *CSGP = malloc(sizeof(CSG));
-    CSGP->course = malloc(sizeof(char)*100);
-    strncpy(CSGP->course, course, 100);
+    CSGP->course = course;
     CSGP->SID = SID;
-    CSGP->grade = malloc(sizeof(char)*100);
-    strncpy(CSGP->grade, grade, 100);
+    CSGP->grade = grade;
     CSGP->next = NULL;
 
     return CSGP;
@@ -17,13 +15,11 @@ CSG *createCSG(char *course, int SID, char *grade) {
 
 //Creates Student ID, Name, Address, and Phone Schema
 //Key: Student ID
-SNAP *createSNAP(int SID, char *name, char *address, int phone) {
+SNAP *createSNAP(int SID, char *name, char *address, char* phone) {
     SNAP *SNAPP = malloc(sizeof(SNAP));
     SNAPP->SID = SID;
-    SNAPP->name = malloc(sizeof(char)*100);
-    strncpy(SNAPP->name, name, 100);
-    SNAPP->address = malloc(sizeof(char)*100);
-    strncpy(SNAPP->address, address, 100);
+    SNAPP->name = name;
+    SNAPP->address = address;
     SNAPP->phone = phone;
     SNAPP->next = NULL;
 
@@ -34,10 +30,8 @@ SNAP *createSNAP(int SID, char *name, char *address, int phone) {
 //Key: Course
 CP *createCP(char *C, char *P) {
     CP *CPP = malloc(sizeof(CP));
-    CPP->course = malloc(sizeof(char)*100);
-    strncpy(CPP->course, C, 100);
-    CPP->prereq = malloc(sizeof(char)*100);
-    strncpy(CPP->prereq, P, 100);
+    CPP->course = C;
+    CPP->prereq = P;
     CPP->next = NULL;
 
     return CPP;
@@ -47,24 +41,20 @@ CP *createCP(char *C, char *P) {
 //Key: Course
 CDH *createCDH(char *C, char *D, char *H) {
     CDH *CDHP = malloc(sizeof(CDH));
-    CDHP->course = malloc(sizeof(char)*100);
-    strncpy(CDHP->course, C, 100);
-    CDHP->day = malloc(sizeof(char)*100);
-    strncpy(CDHP->day, D, 100);
-    CDHP->hour=malloc(sizeof(char)*100);
-    strncpy(CDHP->hour, H, 100);
+    CDHP->course = C;
+    CDHP->day = D;
+    CDHP->hour = H;
     CDHP->next = NULL;
 
     return CDHP;
 }
 
 //Creates Course, Room Schema
+//Key: Course
 CR *createCR(char *C, char *R) {
     CR *CRP = malloc(sizeof(CR));
-    CRP->course=malloc(sizeof(char)*100);
-    strncpy(CRP->course, C, 100);
-    CRP->room=malloc(sizeof(char)*100);
-    strncpy(CRP->room, R, 100);
+    CRP->course = C;
+    CRP->room = R;
     CRP->next = NULL;
 
     return CRP;
