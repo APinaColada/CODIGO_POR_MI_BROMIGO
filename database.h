7f28ffc6c3ae419ac220760dfe9e5874b79bb6ea
APinaColada <38115951+APinaColada@users.noreply.github.com>
2018-04-07 12:57:35 -0400

    Update database.h

--- database.h
+++ database.h
@@ -46,6 +46,26 @@ struct Result_s {
     Tuple_t *tuple;
 };
 
+Database_t *db_create(const char *name);
+void db_destroy(Database_t *db);
+void db_addtable(Database_t *db, const char *name, const char *schema);
+Database_t *db_load(const char *name);
+bool db_dump(Database_t *db);
+bool db_insert(Database_t *db, const char *row, const char *tblname);
+void db_delete(Database_t *db, const char *filter, const char *tblname);
+Result_t *db_lookup(Database_t *db, const char *tuple, const char *tblname);
+Table_t *db_select(Table_t *tbl, const char *cond);
+Table_t *db_gettable(Database_t *db, const char *name);
+Table_t *db_project(Table_t *tbl, char *attrs);
+Table_t *db_join(Table_t *R, Table_t *S);
+
+void table_print(Table_t *tbl);
+void table_destroy(Table_t *tbl);
+
+void print_result(Result_t *res);
+
+void result_destroy(Result_t *res);
+
 /*
 //Creates Course, Student ID, Grade Schema
 //Key: Student ID
