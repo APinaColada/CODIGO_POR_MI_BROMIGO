afc825ec78504c76e181d5685f7acdc4fc9115ae
APinaColada <38115951+APinaColada@users.noreply.github.com>
2018-04-08 19:15:25 -0400

    Update driver.c

--- driver.c
+++ driver.c
@@ -4,7 +4,7 @@ int main() {
     
     Database_t *db = db_create("Main");
     printf("Hey\n");
-    db_addtable(db, "CSG", "CSG");
+    db_addtable(db, "CSG", "Course|SID|Grade");
     printf("Hey\n");
     
 }
