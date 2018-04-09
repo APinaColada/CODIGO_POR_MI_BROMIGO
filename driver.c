8022cbdea22ce60e40b0b7590d5d5ea6782d7cb7
tpauly2 <tpauly2@u.rochester.edu>
2018-04-09 18:32:16 -0400

    Update driver.c

--- driver.c
+++ driver.c
@@ -13,6 +13,16 @@ int main() {
     db_insert(db, "CSG", "CS101|33333|A-");
 
     Table_t *CSG = db_gettable(db, "CSG");
+    
     table_print(CSG);
+    
+    Tuple_t *result = db_lookup(db, "PH100|67890|C+", "CSG", "Course|StudentId|Grade");
+    
+    if (result != NULL) {
+        printf("Target: ");
+        tuple_print(stdout,result);
+    }
+    
     db_destroy(db);
+    
 }
