82f2b4506c1bc317ed490e4bd64d7bf4300fa250
APinaColada <38115951+APinaColada@users.noreply.github.com>
2018-04-12 01:58:30 -0400

    Update database.h

--- database.h
+++ database.h
@@ -43,7 +43,7 @@ void db_delete(Database_t *db, const char *table_name, const char *where);
 Table_t *db_lookup(Table_t *tbl, const char *where);
 Table_t *db_select(Table_t *tbl, const char *cond);
 Table_t *db_gettable(Database_t *db, const char *table_name);
-Table_t *db_project(Table_t *tbl, char *cols);
+Table_t *db_project(Table_t *tbl, char *attrs, char *schema);
 Table_t *db_join(Table_t *R, Table_t *S, const char *name, const char *schema);
 void table_print(Table_t *tbl);
 
